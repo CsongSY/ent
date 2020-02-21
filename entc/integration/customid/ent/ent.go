b61f1c17e17c824fb3ac26b48d437bca5013d49d
Ariel Mashraki <ariel@mashraki.co.il>
2020-02-21 22:31:50 +0200

    entc/hooks: initial work for mutations and hooks

--- entc/integration/customid/ent/ent.go
+++ entc/integration/customid/ent/ent.go
@@ -11,11 +11,21 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/facebookincubator/ent"
 	"github.com/facebookincubator/ent/dialect"
 	"github.com/facebookincubator/ent/dialect/sql"
 	"github.com/facebookincubator/ent/dialect/sql/sqlgraph"
 	"github.com/google/uuid"
 	"golang.org/x/xerrors"
+)
+
+// ent aliases to avoid import conflict in user's code.
+type (
+	Hook       = ent.Hook
+	Value      = ent.Value
+	Mutator    = ent.Mutator
+	Mutation   = ent.Mutation
+	MutateFunc = ent.MutateFunc
 )
 
 // Order applies an ordering on either graph traversal or sql selector.
